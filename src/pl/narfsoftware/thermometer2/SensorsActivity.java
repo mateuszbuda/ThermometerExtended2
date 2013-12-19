e2dd582c5b65ba43951444a8dcbe60b4bd402924
Mateusz Buda <mateusz@narfsoftware.pl>
2013-12-19 14:32:43 +0100

    Settings layouts

--- src/pl/narfsoftware/thermometer2/SensorsActivity.java
+++ src/pl/narfsoftware/thermometer2/SensorsActivity.java
@@ -2,9 +2,11 @@ package pl.narfsoftware.thermometer2;
 
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class SensorsActivity extends Activity implements
 		SensorsFragment.OnSensorSelectedListener {
@@ -53,6 +55,30 @@ public class SensorsActivity extends Activity implements
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.sensors, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			startActivity(new Intent(this, SettingsActivity.class));
+			return true;
+
+		case R.id.action_help:
+			// startActivity(new Intent(this, HelpActivity.class));
+			return true;
+
+		case R.id.action_about:
+			// startActivity(new Intent(this, AboutActivity.class));
+			return true;
+
+		case R.id.action_clear_data:
+
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	@Override
