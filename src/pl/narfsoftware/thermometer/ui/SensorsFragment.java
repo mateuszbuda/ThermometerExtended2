bff872aef1b21543f139ea3807a244536ddf024d
Mateusz Buda <mb921028@gmail.com>
2014-06-19 16:08:28 +0200

    SensorsFragment - updateIcon [bugfix - missing breaks]

--- src/pl/narfsoftware/thermometer/ui/SensorsFragment.java
+++ src/pl/narfsoftware/thermometer/ui/SensorsFragment.java
@@ -26,7 +26,8 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class SensorsFragment extends ListFragment {
+public class SensorsFragment extends ListFragment
+{
 	static final String TAG = "SensorsFragment";
 
 	Activity activity;
@@ -50,7 +51,8 @@ public class SensorsFragment extends ListFragment {
 
 	// The container Activity must implement this interface so the frag can
 	// deliver messages
-	public interface OnSensorSelectedListener {
+	public interface OnSensorSelectedListener
+	{
 		/**
 		 * Called by SensorsFragment when a list item is selected
 		 * 
@@ -61,28 +63,33 @@ public class SensorsFragment extends ListFragment {
 	}
 
 	@Override
-	public void onAttach(Activity activity) {
+	public void onAttach(Activity activity)
+	{
 		super.onAttach(activity);
 		this.activity = activity;
 		// This makes sure that the container activity has implemented
 		// the callback interface. If not, it throws an exception.
-		try {
+		try
+		{
 			callback = (OnSensorSelectedListener) activity;
-		} catch (ClassCastException e) {
+		} catch (ClassCastException e)
+		{
 			throw new ClassCastException(activity.toString()
 					+ " must implement OnSensorSelectedListener");
 		}
 	}
 
 	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
+	public void onActivityCreated(Bundle savedInstanceState)
+	{
 		super.onActivityCreated(savedInstanceState);
 		getListView().setClickable(true);
 		getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 	}
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		app = (ThermometerApp) activity.getApplication();
 		prefs = app.getPrefs();
@@ -95,20 +102,23 @@ public class SensorsFragment extends ListFragment {
 	}
 
 	@Override
-	public void onStart() {
+	public void onStart()
+	{
 		super.onStart();
 
 		// When in two-pane layout, set the listview to highlight the selected
 		// list item
 		// (We do this during onStart because at the point the listview is
 		// available.)
-		if (getFragmentManager().findFragmentById(R.id.sensors_fragment) != null) {
+		if (getFragmentManager().findFragmentById(R.id.sensors_fragment) != null)
+		{
 			getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 		}
 	}
 
 	@Override
-	public void onResume() {
+	public void onResume()
+	{
 		super.onResume();
 
 		initIcons();
@@ -119,14 +129,16 @@ public class SensorsFragment extends ListFragment {
 	}
 
 	@Override
-	public void onPause() {
+	public void onPause()
+	{
 		super.onPause();
 		unregisterListeners();
 		Log.d(TAG, "Sensors unregistered");
 	}
 
 	@Override
-	public void onStop() {
+	public void onStop()
+	{
 		// O.o'?
 		((LinearLayout) getActivity().findViewById(R.id.dateAndTime))
 				.setVisibility(LinearLayout.GONE);
@@ -134,12 +146,14 @@ public class SensorsFragment extends ListFragment {
 	}
 
 	@Override
-	public void onDestroy() {
+	public void onDestroy()
+	{
 		super.onDestroy();
 	}
 
 	@Override
-	public void onListItemClick(ListView l, View v, int position, long id) {
+	public void onListItemClick(ListView l, View v, int position, long id)
+	{
 		v.setSelected(true);
 
 		// Notify the parent activity of selected items' key
@@ -147,57 +161,67 @@ public class SensorsFragment extends ListFragment {
 		SensorRow selected = (SensorRow) getListView().getItemAtPosition(
 				position);
 		for (int key : sensorRows.keySet())
-			if (sensorRows.get(key).equals(selected)) {
+			if (sensorRows.get(key).equals(selected))
+			{
 				selectedItemKey = key;
 				break;
 			}
 
 		callback.onSensorSelected(selectedItemKey);
+		updateIcon(selectedItemKey);
 		// Set the item as checked to be highlighted when in two-pane layout
 		// (tiaa)
 		// getListView().setItemChecked(position, true);
 	}
 
-	private void setAdapter() {
+	private void setAdapter()
+	{
 		sensorsList = new ArrayList<SensorRow>();
 		int key;
-		if (prefs.getShowDataDict().get(key = Sensor.TYPE_AMBIENT_TEMPERATURE)) {
+		if (prefs.getShowDataDict().get(key = Sensor.TYPE_AMBIENT_TEMPERATURE))
+		{
 			sensorRows.put(key,
 					new SensorRow(iconIds.get(key), sensorTitles.get(key),
 							values.get(key)));
 			sensorsList.add(sensorRows.get(key));
 		}
-		if (prefs.getShowDataDict().get(key = Sensor.TYPE_RELATIVE_HUMIDITY)) {
+		if (prefs.getShowDataDict().get(key = Sensor.TYPE_RELATIVE_HUMIDITY))
+		{
 			sensorRows.put(key,
 					new SensorRow(iconIds.get(key), sensorTitles.get(key),
 							values.get(key)));
 			sensorsList.add(sensorRows.get(key));
 		}
-		if (prefs.getShowDataDict().get(key = Sensors.TYPE_ABSOLUTE_HUMIDITY)) {
+		if (prefs.getShowDataDict().get(key = Sensors.TYPE_ABSOLUTE_HUMIDITY))
+		{
 			sensorRows.put(key,
 					new SensorRow(iconIds.get(key), sensorTitles.get(key),
 							values.get(key)));
 			sensorsList.add(sensorRows.get(key));
 		}
-		if (prefs.getShowDataDict().get(key = Sensor.TYPE_PRESSURE)) {
+		if (prefs.getShowDataDict().get(key = Sensor.TYPE_PRESSURE))
+		{
 			sensorRows.put(key,
 					new SensorRow(iconIds.get(key), sensorTitles.get(key),
 							values.get(key)));
 			sensorsList.add(sensorRows.get(key));
 		}
-		if (prefs.getShowDataDict().get(key = Sensors.TYPE_DEW_POINT)) {
+		if (prefs.getShowDataDict().get(key = Sensors.TYPE_DEW_POINT))
+		{
 			sensorRows.put(key,
 					new SensorRow(iconIds.get(key), sensorTitles.get(key),
 							values.get(key)));
 			sensorsList.add(sensorRows.get(key));
 		}
-		if (prefs.getShowDataDict().get(key = Sensor.TYPE_LIGHT)) {
+		if (prefs.getShowDataDict().get(key = Sensor.TYPE_LIGHT))
+		{
 			sensorRows.put(key,
 					new SensorRow(iconIds.get(key), sensorTitles.get(key),
 							values.get(key)));
 			sensorsList.add(sensorRows.get(key));
 		}
-		if (prefs.getShowDataDict().get(key = Sensor.TYPE_MAGNETIC_FIELD)) {
+		if (prefs.getShowDataDict().get(key = Sensor.TYPE_MAGNETIC_FIELD))
+		{
 			sensorRows.put(key,
 					new SensorRow(iconIds.get(key), sensorTitles.get(key),
 							values.get(key)));
@@ -207,36 +231,42 @@ public class SensorsFragment extends ListFragment {
 		adapter = new SensorsListViewAdapter(getActivity(),
 				R.layout.sensor_row, sensorsList);
 
-		adapter.registerDataSetObserver(new DataSetObserver() {
+		adapter.registerDataSetObserver(new DataSetObserver()
+		{
 		});
 		getListView().setDivider(null);
 		setListAdapter(adapter);
 	}
 
-	private void initListeners() {
+	private void initListeners()
+	{
 		UIListenersFactory factory = new UIListenersFactory(activity, adapter,
 				sensorRows);
 		for (int key : sensorTitles.keySet())
 			listeners.put(key, factory.createListener(key));
 	}
 
-	private void registerChosenListeners() {
+	private void registerChosenListeners()
+	{
 		for (int key : listeners.keySet())
 			if (prefs.getShowDataDict().get(key))
 				listeners.get(key).register();
 	}
 
-	private void unregisterListeners() {
+	private void unregisterListeners()
+	{
 		for (int key : listeners.keySet())
 			listeners.get(key).unregister();
 	}
 
-	private void init() {
+	private void init()
+	{
 		initSensorTitles();
 		initValues();
 	}
 
-	void initIcons() {
+	void initIcons()
+	{
 		iconIds.put(
 				Sensor.TYPE_AMBIENT_TEMPERATURE,
 				app.saveData(Sensor.TYPE_AMBIENT_TEMPERATURE) ? R.drawable.temprature
@@ -264,7 +294,8 @@ public class SensorsFragment extends ListFragment {
 						: R.drawable.magnetic_field_disabled);
 	}
 
-	private void initValues() {
+	private void initValues()
+	{
 		HashMap<Integer, Boolean> dict = app.getSensors().hasSensor;
 		for (int key : dict.keySet())
 			values.put(key, dict.get(key) ? noData : sensorUnavailavle);
@@ -281,7 +312,8 @@ public class SensorsFragment extends ListFragment {
 						: sensorUnavailavle);
 	}
 
-	private void initSensorTitles() {
+	private void initSensorTitles()
+	{
 		Resources res = activity.getResources();
 		sensorTitles.put(Sensor.TYPE_AMBIENT_TEMPERATURE,
 				res.getString(R.string.ambient_temp_title));
@@ -299,7 +331,8 @@ public class SensorsFragment extends ListFragment {
 				res.getString(R.string.magnetic_field_title));
 	}
 
-	private void initDateAndTime() {
+	private void initDateAndTime()
+	{
 		LinearLayout dateAndTime = ((LinearLayout) activity
 				.findViewById(R.id.dateAndTime));
 		TextView date = (TextView) activity.findViewById(R.id.date);
@@ -308,18 +341,21 @@ public class SensorsFragment extends ListFragment {
 		time.setTypeface(prefs.getTypeface());
 
 		if (prefs.getDateFormat().equals("")
-				&& prefs.getTimeFormat().equals("")) {
+				&& prefs.getTimeFormat().equals(""))
+		{
 			dateAndTime.setVisibility(LinearLayout.GONE);
 			return;
 		} else if (prefs.getDateFormat().equals("")
-				&& !prefs.getTimeFormat().equals("")) {
+				&& !prefs.getTimeFormat().equals(""))
+		{
 			dateAndTime.setVisibility(LinearLayout.VISIBLE);
 			date.setVisibility(LinearLayout.GONE);
 			time.setText(new SimpleDateFormat(prefs.getTimeFormat(), Locale
 					.getDefault()).format(new Date().getTime()));
 			return;
 		} else if (prefs.getTimeFormat().equals("")
-				&& !prefs.getDateFormat().equals("")) {
+				&& !prefs.getDateFormat().equals(""))
+		{
 			dateAndTime.setVisibility(LinearLayout.VISIBLE);
 			time.setVisibility(LinearLayout.GONE);
 			date.setText(new SimpleDateFormat(prefs.getDateFormat(), Locale
@@ -337,37 +373,48 @@ public class SensorsFragment extends ListFragment {
 				.getDefault()).format(new Date().getTime()));
 	}
 
-	public void updateSensorsFragment(int key) {
+	public void updateSensorsFragment(int key)
+	{
 		updateIcon(key);
 	}
 
-	private void updateIcon(int key) {
+	private void updateIcon(int key)
+	{
 		int iconId = 0;
-		switch (key) {
+		switch (key)
+		{
 		case Sensor.TYPE_AMBIENT_TEMPERATURE:
 			iconId = app.saveData(key) ? R.drawable.temprature
 					: R.drawable.temprature_disabled;
+			break;
 		case Sensor.TYPE_RELATIVE_HUMIDITY:
 			iconId = app.saveData(key) ? R.drawable.relative_humidity
 					: R.drawable.relative_humidity_disabled;
+			break;
 		case Sensor.TYPE_PRESSURE:
 			iconId = app.saveData(key) ? R.drawable.pressure
 					: R.drawable.pressure_disabled;
+			break;
 		case Sensor.TYPE_LIGHT:
 			iconId = app.saveData(key) ? R.drawable.light
 					: R.drawable.light_disabled;
+			break;
 		case Sensor.TYPE_MAGNETIC_FIELD:
 			iconId = app.saveData(key) ? R.drawable.magnetic_field
 					: R.drawable.magnetic_field_disabled;
+			break;
 		case Sensors.TYPE_ABSOLUTE_HUMIDITY:
 			iconId = app.saveData(key) ? R.drawable.absolute_humidity
 					: R.drawable.absolute_humidity_disabled;
+			break;
 		case Sensors.TYPE_DEW_POINT:
 			iconId = app.saveData(key) ? R.drawable.dew_point
 					: R.drawable.dew_point_disabled;
+			break;
 		}
 
-		if (adapter == null) {
+		if (adapter == null)
+		{
 			adapter = (SensorsListViewAdapter) getListAdapter();
 			if (adapter == null)
 				return;
@@ -375,7 +422,8 @@ public class SensorsFragment extends ListFragment {
 
 		SensorRow sensorRow = sensorRows.get(key);
 
-		if (sensorRow != null) {
+		if (sensorRow != null)
+		{
 			sensorRow.setIcon(iconId);
 			adapter.notifyDataSetChanged();
 		}
